c2c97ddc2a43fcd00dd2010a66bdb05a2bf3c32f
Georgi Georgiev <gkalchv@icloud.com>
2022-11-26 23:27:00 +0200

    added added more images

---
+++ src/components/sports-section/SportsSection.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import NftExample from "../NFTExample/NFTExample"
+import { StaticImageData } from "next/image"
+
+const SportsSection: FC<{ width: number, title: string, sources: StaticImageData[] }> = ({sources, title, width}) => {
+  return (
+    <div className={'flex flex-col justify-center items-center px-10 mt-96 pt-30 border'}>
+      <h1 className={'light'}>{title}</h1>
+      <div className={'pt-36'}>
+      <div className={`columns-${sources.length} gap-28 `}>
+        {sources.map((source, index) => {
+          return <NftExample key={index} width={width} source={source} />
+        })}
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default SportsSection;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,19 @@ import styles from "./index.module.css";
 import { type NextPage } from "next";
 import Head from "next/head";
 import NftExample from "../components/NFTExample/NFTExample"
-import nft from '../../public/2.png'
+import nft1 from '../../public/1.png'
+import nft2 from '../../public/2.png'
+import nft3 from '../../public/3.png'
+import nft4 from '../../public/4.png'
+import nft5 from '../../public/5.png'
+import nft6 from '../../public/6.png'
+import nft7 from '../../public/7.png'
+import nft8 from '../../public/8.png'
+import nft9 from '../../public/9.png'
+import nft10 from '../../public/10.png'
+import nft11 from '../../public/11.png'
 import Link from "next/link"
+import SportsSection from "../components/sports-section/SportsSection"
 
 const Home: NextPage = () => {
   return (
@@ -15,7 +26,6 @@ const Home: NextPage = () => {
       </Head>
       <main className={styles.main}>
         <div className={styles.container}>
-          <div className={'pt-36'}>navigation</div>
           <div className={'columns-2'}>
             <div className='col'>
               <h1>THE MONSTERS ARE BACK IN TOWN</h1>
@@ -24,35 +34,13 @@ const Home: NextPage = () => {
                 <button className={'primary-button'}>MINT NFT</button>
               </Link>
             </div>
-            <div><NftExample width={600} source={nft} /></div>
+            <div><NftExample width={600} source={nft2} /></div>
           </div>
-          <div className={'flex flex-col justify-center items-center px-10 mt-96 pt-30 border'}>
-            <h1 className={'light'}>WINTER SPORTS</h1>
-            <div className='columns-3 gap-28 pt-36'>
-              <NftExample width={600} source={nft} />
-              <NftExample width={600} source={nft} />
-              <NftExample width={600} source={nft} />
-            </div>
-          </div>
-          <div className={'flex flex-col justify-center items-center px-10 mt-96 pt-30 border'}>
-            <h1 className={'light'}>Professional sports</h1>
-            <div className='columns-3 gap-28 pt-36'>
-              <NftExample width={600} source={nft} />
-              <NftExample width={600} source={nft} />
-              <NftExample width={600} source={nft} />
-            </div>
-          </div>
-          <div className={'flex flex-col justify-center items-center px-10 mt-96 pt-30 border'}>
-            <h1 className={'light'}>Water sports</h1>
-            <div className='columns-3 gap-28 pt-36'>
-              <NftExample width={600} source={nft} />
-              <NftExample width={600} source={nft} />
-              <NftExample width={600} source={nft} />
-            </div>
-            <div className='columns-1 gap-28 pt-36'>
-              <NftExample width={400} source={nft}/>
-            </div>
-          </div>
+          <SportsSection sources={[nft1, nft3, nft4]} width={600} title='Winter Sports' />
+          <SportsSection sources={[nft11, nft9, nft10,]} width={600} title='Professional sports' />
+          <SportsSection sources={[nft6, nft5, nft8,]} width={600} title='Water sports' />
+          <SportsSection sources={[nft7]} width={400} title='' />
+
         </div>
         <div className={'py-36'}>
           allr
